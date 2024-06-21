dfe4d493ae1233a6f13955aac2a1529c59437a79
watcharawalee24 <watcharawalee.ju@mail.wu.ac.th>
2024-06-21 14:01:50 +0700

    pages

---
+++ pages/ListVD/LV.vue
@@ -0,0 +1,93 @@
+
+<template>
+    <div class="navbar navbar-light bg-light ">
+      <h4><a class="navbar-brand">คลังความรู้</a></h4>
+  
+      <div class="search_two">
+        <div class="input-searchh">
+          <div class="icon">
+            <span class="fa fa-search"></span>
+          </div>
+          <input type="search" name="search" id="" placeholder="ค้นหาเรื่องที่ต้องการ">
+        </div>
+      </div>
+    </div>
+  
+  
+    <!-- 🏳️‍🌈ส่วนแสดง video🏳️‍🌈 -->
+    <div class="headerv">
+          <h3>การพัฒนาบุคลากรข้าราชการไทย</h3>
+          <p><i class='fas fa-book-open' style="color: goldenrod;"></i> หลักการจัดโครงสร้างการบริหารราชการไทย</p>
+         </div>
+        <div class="video" id="videoContainer">
+            <video id="myVideo" :src="videoUrl"  width="full" height="600" poster="/img/PosterV.png" controls>
+                <!-- <source src="/assets/video/v1.mp4" type="video/mp4"> -->
+            </video>
+            <!-- <i class="fas fa-play play-pause" id="playPauseIcon"></i> -->
+        </div>
+    <div class="bod">
+      <h3>บทที่ 1 </h3>
+      <p>พัฒนาบุคลากรข้าราชการไทย</p>
+    </div>
+    <div class="button-container">
+      <button class="pretest-button">
+      <i class="far fa-file-alt"></i><span class="hover-text">แบบทดสอบก่อนเรียน</span>
+    </button>
+    <div class="share-container">
+      <button id="shareButton" style="font-size: small;"><i class="fas fa-share"></i> แชร์</button>
+      <ul id="shareOptions" class="share-options">
+        <li><a href="#" id="copyLink"><i class="fas fa-copy"></i> คัดลอกลิงค์</a></li>
+        <li><a href="#" id="shareFacebook"><i class="fab fa-facebook-square"></i> แชร์ไปยังเฟสบุ๊ค</a></li>
+        <li><a href="#" id="shareTwitter"><i class="fab fa-twitter"></i> แชร์ไปยังทวิตเตอร์</a></li>
+      </ul>
+    </div>
+  </div>
+    
+  
+  
+  
+  
+  
+  
+    <!-- 🍓ส่วน List video 🍓-->
+    <div class="container_VD">
+      <div class="video-item" v-for="item in items">
+        <div class="thumbnail">
+          <div class="img_VD">
+            <img :src="'/img/' + item.image" alt="">
+            <!-- <img src="/img/cardKL1.jpg" alt=""> -->
+          </div>
+        </div>
+        <div class="details">
+          <h3>{{ item.subject }}</h3>
+          <h7>{{ item.name }}</h7>
+          <p>กลุ่มงาน บค.การรับข้าราชการใหม่</p>
+          <p><a href="javascript:void(0);" @click="playVideo(item.video)">เข้าชม</a></p>
+        </div>
+      </div>
+    </div>
+  
+  
+   
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from 'vue';
+  
+  const items = ref(
+  [
+    { id: '1', subject: 'บทที่ 1', name: 'ข้าราชการใหม่1', image: 'cardKL1.jpg', video: 'v1.mp4'},
+    { id: '2', subject: 'บทที่ 2', name: 'ข้าราชการใหม่2', image: 'cardKL2.jpg', video: 'v2.mp4' },
+    { id: '3', subject: 'บทที่ 3', name: 'ข้าราชการใหม่3', image: 'cardKL3.jpg', video: 'v2.mp4' },
+  
+  ])
+  
+  const videoUrl = ref("/video/v1.mp4");
+  
+  // Function to change video URL
+  const playVideo = (videoSrc: string) => {
+    videoUrl.value = "/video/" + videoSrc;
+  };
+  
+  </script>
+  
---
+++ pages/index.vue
@@ -0,0 +1,195 @@
+<template>
+    <!-- 🌷Branner🌷r -->
+    <div class="pp">
+        <div class="poster-container">
+            <div class="poster">
+                <img src="/img/branner.jpg" alt="">
+            </div>
+        </div>
+    </div>
+
+    <!-- 🌷Toppic News🌷 -->
+    <div class="News">
+        <h3>ข่าวสารน่าสนใจ</h3>
+    </div>
+
+    <!-- 🌷News Slide🌷 -->
+    <div class="container">
+    <div class="card-carousel-container">
+      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="/img/News1.jpg" class="d-block w-100" alt="Slide 1">
+          </div>
+          <div class="carousel-item">
+            <img src="/img/News2.jpg" class="d-block w-100" alt="Slide 2">
+          </div>
+          <div class="carousel-item">
+            <img src="/img/News3.jpg" class="d-block w-100" alt="Slide 3">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  </div>
+   
+    
+    <!-- 🌷TOPPIC GROUP WORK🌷 -->
+    <div class="groupwork" id="groupwork">
+        <h3>กลุ่มงาน</h3>
+    </div>
+
+    <div class="sl-bt">
+        <button id="pre-sl" class="press">&laquo;</button>
+        <div class="card-g">
+            <div class="card-main">
+                <div class="card-img">
+                    <img src="/img/GroupWork.jpg" alt="">
+                    <li><a href="/G1">◉ กลุ่มงานอัตรากำลัง</a></li>
+                </div>
+            </div>
+            <div class="card-main">
+                <div class="card-img">
+                    <img src="/img/GroupWork.jpg" alt="">
+                    <li><a href="">◉ กลุ่มภารกิจอำนวยการ</a></li>
+                </div>
+            </div>
+            <div class="card-main">
+                <div class="card-img">
+                    <img src="/img/GroupWork.jpg" alt="">
+                    <li><a href="">◉ กลุ่มงานสรรหาบุคคล </a></li>
+                </div>
+            </div>
+            <div class="card-main">
+                <div class="card-img">
+                    <img src="/img/GroupWork.jpg" alt="">
+                    <li><a href="">◉ กลุ่มงานบริหารและพัฒนารูปแบบการจ้างงานทางเลือก</a></li>
+                </div>
+            </div>
+            <div class="card-main">
+                <div class="card-img">
+                    <img src="/img/GroupWork.jpg" alt="">
+                    <li><a href="">◉ กลุ่มงานยุทธศาสตร์และมาตรฐานด้านบริหารทรัพยากรบุคคล</a></li>
+                </div>
+            </div>
+            <div class="card-main">
+                <div class="card-img">
+                    <img src="/img/GroupWork.jpg" alt="">
+                    <li><a href="">◉ กลุ่มงานบรรจุแต่งตั้ง</a></li>
+                </div>
+            </div>
+            <div class="card-main">
+                <div class="card-img">
+                    <img src="/img/GroupWork.jpg" alt="">
+                    <li><a href="">◉ กลุ่มงานทะเบียนประวัติและเครื่องราชอิสริยาภรณ์ </a></li>
+                </div>
+            </div>
+            <div class="card-main">
+                <div class="card-img">
+                    <img src="/img/GroupWork.jpg" alt="">
+                    <li><a href="">◉ กลุ่มงานประเมินบุคคลและวิชาการ</a></li>
+                </div>
+            </div>
+            <div class="card-main">
+                <div class="card-img">
+                    <img src="/img/GroupWork.jpg" alt="">
+                    <li><a href="">◉ กลุ่มงานเงินเดือนและตรวจสอบประเมินผลกำลังคน</a></li>
+                </div>
+            </div>
+            <div class="card-main">
+                <div class="card-img">
+                    <img src="/img/GroupWork.jpg" alt="">
+                    <li><a href="">◉ กลุ่มงานสร้างความต่อเนื่องทางการบริหาร</a></li>
+                </div>
+            </div>
+            <div class="card-main">
+                <div class="card-img">
+                    <img src="/img/GroupWork.jpg" alt="">
+                    <li><a href="">◉ กลุ่มงานบริหารผลการปฏิบัติราชการและเทคโนโลยีสารสนเทศด้านบริหารทรัพยากรบุคคล</a></li>
+                </div>
+            </div>
+        </div>
+        <button id="next-sl" class="press">&raquo;</button>
+    </div>
+
+    <!-- 🌷VideoCard🌷 -->
+    <div class="Videocard">
+        <h3>วิดีโอแนะนำ</h3>
+    </div>
+
+    <div class="blog">
+        <div class="main">
+            <div class="img">
+                <img src="" alt="">
+            </div>
+            <div class="content">
+                <h3>การจัดสรร</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <a href="#" class="LeanBTN">เข้าชม</a>
+            </div>
+        </div>
+        <div class="main">
+            <div class="img">
+                <img src="" alt="">
+            </div>
+            <div class="content">
+                <h3>การจัดสรร</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <a href="#" class="LeanBTN">เข้าชม</a>
+            </div>
+        </div>
+        <div class="main">
+            <div class="img">
+                <img src="" alt="">
+            </div>
+            <div class="content">
+                <h3>การจัดสรร</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <a href="#" class="LeanBTN">เข้าชม</a>
+            </div>
+        </div>
+        <div class="main">
+            <div class="img">
+                <img src="" alt="">
+            </div>
+            <div class="content">
+                <h3>การจัดสรร</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <a href="#" class="LeanBTN">เข้าชม</a>
+            </div>
+        </div>
+        <div class="main">
+            <div class="img">
+                <img src="" alt="">
+            </div>
+            <div class="content">
+                <h3>การจัดสรร</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <a href="#" class="LeanBTN">เข้าชม</a>
+            </div>
+        </div>
+        <div class="main">
+            <div class="img">
+                <img src="" alt="">
+            </div>
+            <div class="content">
+                <h3>การจัดสรร</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <a href="#" class="LeanBTN">เข้าชม</a>
+            </div>
+        </div>
+    </div>
+</template>
+
